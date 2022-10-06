c79e592c41b0fb7ff7bcdf90f1afb6e9420cfdcc
Danny Festor <danny.festor@gmail.com>
2022-10-06 14:30:59 +0900

    Update Styles and Scripts

--- ui/assets/js/app.js
+++ ui/assets/js/app.js
@@ -1,12 +1,16 @@
 import "../css/app.scss";
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('#hamburger').addEventListener('click', () => {
-        const bars = document.querySelectorAll('.hamburger-bar');
-        bars.forEach((bar) => {
-            bar.classList.toggle('open');
-        })
-
-        document.querySelector('#navigation').classList.toggle('open');
+const handleHamburgerClicked = () => {
+    const bars = document.querySelectorAll('.hamburger-bar');
+    bars.forEach((bar) => {
+        bar.classList.toggle('open');
     })
+
+    document.querySelector('#navigation').classList.toggle('open');
+    document.querySelector('#backdrop').classList.toggle('open');
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('#hamburger').addEventListener('click', handleHamburgerClicked)
+    document.querySelector('#backdrop').addEventListener('click', handleHamburgerClicked)
 });
