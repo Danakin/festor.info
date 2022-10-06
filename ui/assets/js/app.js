55b5a738d7273a41e71c195c7269dabfa8486533
Danny Festor <danny.festor@gmail.com>
2022-10-06 14:16:45 +0900

    Add Hamburger

--- ui/assets/js/app.js
+++ ui/assets/js/app.js
@@ -1,1 +1,12 @@
 import "../css/app.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('#hamburger').addEventListener('click', () => {
+        const bars = document.querySelectorAll('.hamburger-bar');
+        bars.forEach((bar) => {
+            bar.classList.toggle('open');
+        })
+
+        document.querySelector('#navigation').classList.toggle('open');
+    })
+});
