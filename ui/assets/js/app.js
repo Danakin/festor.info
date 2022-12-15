0cfeb63432f556a0943437119a480ff7037ba036
Danny Festor <danny.festor@gmail.com>
2022-12-15 19:18:47 +0900

    Fix Hamburger Breakpoint

--- ui/assets/js/app.js
+++ ui/assets/js/app.js
@@ -11,6 +11,22 @@ const handleHamburgerClicked = () => {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('#hamburger').addEventListener('click', handleHamburgerClicked)
-    document.querySelector('#backdrop').addEventListener('click', handleHamburgerClicked)
+    document.querySelector('#hamburger').addEventListener('click', handleHamburgerClicked);
+    document.querySelector('#backdrop').addEventListener('click', handleHamburgerClicked);
+
+    window.addEventListener('resize', (e) => {
+        const navigation = document.querySelector('#navigation')
+        const backdrop = document.querySelector('#backdrop')
+        const bars = document.querySelectorAll('.hamburger-bar');
+
+        if (e.target.innerWidth >= 700 && navigation.classList.contains('open')) {
+            navigation.classList.remove('open');
+            backdrop.classList.remove('open');
+            bars.forEach((bar) => {
+                bar.classList.remove('open');
+            })
+        }
+        // document.querySelector('#navigation').classList.toggle('open');
+        // document.querySelector('#backdrop').classList.toggle('open');
+    })
 });
